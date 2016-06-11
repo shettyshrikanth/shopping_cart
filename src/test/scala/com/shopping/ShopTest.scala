93deca98f061d333d496fbed9422ffc99fcd720c
shettyshrikanth <skrshetty@hotmail.com>
2016-06-11 23:23:30 +0100

    ShopTest Refactored

--- src/test/scala/com/shopping/ShopTest.scala
+++ src/test/scala/com/shopping/ShopTest.scala
@@ -9,35 +9,28 @@ class ShopTest extends Specification {
 
   "Shop which only sells apples and oranges " should {
     "Sell only Apples And Oranges" in {
-      val size = Shop(Apple, Orange) filter { case item @ (Apple | Orange) => false; case _ => false } size
-
-      size mustEqual 0
+      Shop(Apple, Orange) must contain(exactly(Apple, Orange))
     }
   }
-  
-    "Apples cost 60p and Oranges cost 25p, therefore shop " should {
-    "Sell Apple for 60p each " in {
-      val appleList = Shop(Apple, Orange) filter { case item @ Apple => true; case _ => false }
 
-      appleList(0).price mustEqual 0.60
+  "Apples cost 60p and Oranges cost 25p, therefore shop " should {
+    "Sell Apple for 60p each " in {
+      Shop(Apple, Orange).head.price mustEqual 0.60
     }
 
     "Sell Orange for 25p each " in {
-      val orangeList = Shop(Apple, Orange) filter { case item @ Orange => true; case _ => false }
-
-      orangeList(0).price mustEqual 0.25
+      Shop(Apple, Orange)(1).price mustEqual 0.25
     }
   }
-    
-    
+
   "Checkout system which takes a list of items scanned at the till and outputs the total cost" should {
 
     "3 Apples and 1 Orange cost Sterling 2.05  " in {
       Shop.checkout(ShoppingCart(Apple, Apple, Apple, Orange)) mustEqual 2.05
     }
-    
+
     " Empty shopping cart cost 0.00 " in {
-      Shop.checkout(ShoppingCart()) mustEqual 0.0       
+      Shop.checkout(ShoppingCart()) mustEqual 0.0
     }
   }
 }
